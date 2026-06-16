c61a989b9f0cc1fee610a806a05191916ca75c16
homebrew-releaser <homebrew-releaser@example.com>
2026-06-16 09:03:48 +0000

    chore: brew formula update for annot v0.12.0

--- Formula/annot.rb
+++ Formula/annot.rb
@@ -5,15 +5,15 @@
 class Annot < Formula
   desc "Human-in-the-loop annotation tool for ai workflows"
   homepage "https://github.com/denolehov/annot"
-  url "https://github.com/denolehov/annot/archive/refs/tags/v0.11.0.tar.gz"
-  version "0.11.0"
-  sha256 "0eadd8fc7286ef71238d07b221b8c646d8cf7632a470c03450085b32c8164be7"
+  url "https://github.com/denolehov/annot/archive/refs/tags/v0.12.0.tar.gz"
+  version "0.12.0"
+  sha256 "46d8e227f732b98a47a57a43a6501f9a755fb45350b022261a1e59f7cb64ff4c"
   license "AGPL-3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/denolehov/annot/releases/download/v0.11.0/annot-0.11.0-darwin-arm64.tar.gz"
-      sha256 "5529b1dc4252c7a77e2cb040fb32d7a867aea4b274016e7d51e2935a50765aaf"
+      url "https://github.com/denolehov/annot/releases/download/v0.12.0/annot-0.12.0-darwin-arm64.tar.gz"
+      sha256 "ae395f8ef4678f91b57c5a63506240ab0be5bf922e75c919359c26c8d88f75e7"
     end
   end
 
